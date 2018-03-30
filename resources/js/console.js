f62570c4ccaab3e09effd5673a8a20b37e19fa71
Miguelmo <miguelmoyaortega@gmail.com>
2018-03-30 12:03:56 +0200

    nuevo formato

--- resources/js/console.js
+++ resources/js/console.js
@@ -1,9 +1,32 @@
-var divConsole = document.getElementById('console'), firstText = 'Miguelmo@home:~$ ';
-function createNewLine(toInsert) {
-    var newSpan = document.createElement('span');
+let divConsole = document.getElementById('console'), // Elemento de la consola
+firstText = 'Miguelmo@home:~$ ', actualSpan, textToWrite, index = 0;
+let createNewLine = (toInsert) => {
+    let newSpan = document.createElement('span');
     newSpan.textContent = toInsert || '';
     divConsole.appendChild(newSpan);
+    actualSpan = newSpan;
     return newSpan;
-}
-;
+};
+let writeText = (text) => {
+    textToWrite = text;
+    let promise = new Promise(resolve => {
+        let i, time = 300;
+        for (i = 0; i < textToWrite.length; i++) {
+            setTimeout(() => {
+                actualSpan.textContent += textToWrite[index];
+                index++;
+            }, i * time);
+        }
+        setTimeout(() => {
+            index = 0;
+            resolve();
+        }, i * time);
+    });
+    return promise;
+};
+//LÓGICA DEL CODIGO
 createNewLine(firstText);
+createNewLine(firstText);
+writeText('hola').then(() => {
+    console.log('fin');
+});
--- src/resources/ts/console.ts
+++ src/resources/ts/console.ts
@@ -1,15 +1,38 @@
-// outdir: ../../../resources/js/
-
 let divConsole = document.getElementById('console'),        // Elemento de la consola
-    firstText = 'Miguelmo@home:~$ ';                        // Texto inicial
+    firstText = 'Miguelmo@home:~$ ',
+    actualSpan: HTMLSpanElement,
+    textToWrite: string, index = 0;
 
-function createNewLine(toInsert: string) {
+let createNewLine = (toInsert: string) => {
     let newSpan = document.createElement('span');
     newSpan.textContent = toInsert || '';
     divConsole.appendChild(newSpan);
+    actualSpan = newSpan;
     return newSpan;
+};
+
+let writeText = (text: string) => {
+    textToWrite = text;
+    let promise = new Promise(resolve => {
+        let i, time = 300;
+        for (i = 0; i < textToWrite.length; i++) {
+            setTimeout(() => {
+                actualSpan.textContent += textToWrite[index];
+                index++;
+            }, i * time);
+        }
+        setTimeout(() => {
+            index = 0;
+            resolve();
+        }, i * time);
+    });
+    return promise;
 };
 
 
 //LÓGICA DEL CODIGO
 createNewLine(firstText);
+createNewLine(firstText);
+writeText('hola').then(() => {
+    console.log('fin');
+});
