bc861be77f613788e956e66eb5687db01fc120ab
Miguelmo <miguelmoyaortega@gmail.com>
2018-03-30 12:44:46 +0200

    primera prueba

--- resources/js/console.js
+++ resources/js/console.js
@@ -1,32 +1,46 @@
 let divConsole = document.getElementById('console'), // Elemento de la consola
 firstText = 'Miguelmo@home:~$ ', actualSpan, textToWrite, index = 0;
-let createNewLine = (toInsert) => {
+let newLine = () => {
     let newSpan = document.createElement('span');
-    newSpan.textContent = toInsert || '';
+    newSpan.textContent = firstText || '';
     divConsole.appendChild(newSpan);
     actualSpan = newSpan;
     return newSpan;
 };
-let writeText = (text) => {
+let writeText = (text, time) => {
     textToWrite = text;
     let promise = new Promise(resolve => {
-        let i, time = 300;
+        let i, milliseconds = time || 50;
         for (i = 0; i < textToWrite.length; i++) {
             setTimeout(() => {
                 actualSpan.textContent += textToWrite[index];
                 index++;
-            }, i * time);
+            }, i * milliseconds);
         }
         setTimeout(() => {
             index = 0;
             resolve();
-        }, i * time);
+        }, i * milliseconds);
     });
     return promise;
 };
 //LÓGICA DEL CODIGO
-createNewLine(firstText);
-createNewLine(firstText);
-writeText('hola').then(() => {
-    console.log('fin');
+newLine();
+writeText('Bienvenid@ a mi web').then(() => {
+    newLine();
+    writeText('Datos:').then(() => {
+        newLine();
+        writeText('Sant Vicent del Raspeig, Comunidad Valenciana, España.', 30).then(() => {
+            newLine();
+            writeText('miguelmoyaortega@hotmail.com', 30).then(() => {
+                newLine();
+                writeText('github.com/miguelmoya1 isofocus.es', 30).then(() => {
+                    newLine();
+                    //     writeText('Datos:').then(() => {
+                    //         newLine();
+                    //     });
+                });
+            });
+        });
+    });
 });
